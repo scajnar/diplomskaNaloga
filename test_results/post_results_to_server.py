5d43a14170033e4058c5524518b39b9406d0d80d
samo <scajnar@yahoo.com>
2023-04-14 13:00:08 +0200

    Some minor clean ups

--- test_results/post_results_to_server.py
+++ test_results/post_results_to_server.py
@@ -1,5 +1,4 @@
-import os
-import subprocess
+
 import sys
 
 import pytest
@@ -14,15 +13,14 @@ def setup_backend():
     return backend
 
 def run_pytest_and_get_results():
-    # Run pytest and generate .xml output
+    # Run pytest with JUnit and generate .xml output
     pytest_command = "pytest --junitxml=output.xml"
     result = subprocess.run(pytest_command, shell=True, check=False)
 
     if result.returncode not in (0, 1):
         print(f"Error: pytest returned non-zero exit status {result.returncode}")
         sys.exit(1)
-    # Parse .xml output and create a list of test results
-
+    # Parse .xml output using JUnit and create a list of test results
     junit_xml = JUnitXml.fromfile("output.xml")
     test_results = []
 
@@ -40,7 +38,6 @@ def run_pytest_and_get_results():
                 status = "PASSED"
             test_function_name = case.name.split("::")[-1]  # Extract only the test function name
             result = {
-                # "summary": f"{suite.name}::{case.name}",
                 "summary": test_function_name,
                 "status": status,
                 "comment": str(case.result) if case.result else "",
@@ -51,43 +48,13 @@ def run_pytest_and_get_results():
     os.remove("output.xml")
 
     return test_results
-'''
-def run_pytest_and_tcms():
-    os.environ["SSL_CERT_FILE"] = "/home/samo/diplomskaNaloga/kiwi/ca.crt"
-    os.environ["JOB_NAME"] = "PYTHON TEST 12.4 11.00 NO error"
-    os.environ["TCMS_PRODUCT_VERSION"] = "1"
-    os.environ["BUILD_NUMBER"] = "1"
-    os.environ["TCMS_USERNAME"] = "samo"
-    os.environ["TCMS_PASSWORD"] = "samo"
-    subprocess.run(["pytest", "--junit-xml=./report.xml"])
-    subprocess.run(["tcms-junit.xml-plugin", "./report.xml"])
-    # Provide your KiwiTCMS username and password
-    backend = setup_backend()
 
-    # Run pytest and get the results in a dictionary or list format
-    # (you'll need to implement this part)
-    test_results = run_pytest_and_get_results()
-
-    for test_result in test_results:
-        test_case, was_created = backend.test_case_get_or_create(test_result["summary"])
-        print(f'ID JE {test_case["id"]}, created {was_created}')
-        backend.add_test_case_to_plan(test_case["id"], backend.plan_id)
-        test_executions = backend.add_test_case_to_run(test_case["id"], backend.run_id)
-
-        status_id = backend.get_status_id(test_result["status"])
-        print(f'RESULT JE {test_result}')
-        for execution in test_executions:
-            backend.update_test_execution(execution["id"], status_id, test_result["comment"])
-
-    backend.finish_test_run()
-    # Upload test results to KiwiTCMS
-'''
 test_case_mapping = {
     "test_01": 1,
     "test_02": 2,
     "test_03": 3,
     "test_04": 4,
-    # Add more mappings as needed
+    # Add more mappings as needed, numbers correspond to test case IDs in KiwiTCMS https://164.90.161.164/cases/search/
 }
 
 def run_pytest_and_tcms():
@@ -103,11 +70,8 @@ def run_pytest_and_tcms():
     os.environ["TCMS_BUILD"] = "bf75d0a"
     os.environ["TCMS_USERNAME"] = "samo"
     os.environ["TCMS_PASSWORD"] = "samo"
-    # subprocess.run(["pytest", "--junit-xml=./report.xml"])
-    # subprocess.run(["tcms-junit.xml-plugin", "./report.xml"])
-    # Provide your KiwiTCMS username and password
+
     backend = setup_backend()
-    # backend.plan_id = 46
     test_results = run_pytest_and_get_results()
 
     for test_result in test_results:
